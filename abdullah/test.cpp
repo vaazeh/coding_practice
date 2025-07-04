f74570939fa6d129656f94341d9b1869561ec012
HafizAbdullah998 <hafiz.abdullahziad@gmail.com>
2025-07-04 15:46:51 +0500

    update file single Argument to multiple Argument

---
+++ abdullah/test.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+int main(int argc, char* argv[])
+{
+    if(argc >=2)
+    cout<<argc;
+    int i=0;
+    while(i<argc){
+        cout<<argv[i]<<endl;
+        i++;
+
+    }
+}
